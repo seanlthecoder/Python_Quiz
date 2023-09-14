4ed532c3ee3447778ffebdb4885f38c79d1a4fde
Sean Lester Durham <sean.durham1982@gmail.com>
2023-09-13 23:08:00 -0500

    updated python files

---
+++ quizPython/quizz.py
@@ -0,0 +1,41 @@
+print("Welcome to my computer quiz")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("okay! Let's play :")
+
+answer = input("what does CPU stand for? ")
+
+if answer == "central processing unit":
+    print('Correct!')
+
+else:
+    print("Incorrect")
+
+answer = input("what does CPU stand for? ")
+
+if answer == "central processing unit":
+    print('Correct!')
+
+else:
+    print("Incorrect")
+
+    answer = input("what does CPU stand for? ")
+
+if answer == "central processing unit":
+    print('Correct!')
+
+else:
+    print("Incorrect")
+
+
+    answer = input("what does CPU stand for? ")
+
+if answer == "central processing unit":
+    print('Correct!')
+
+else:
+    print("Incorrect")
